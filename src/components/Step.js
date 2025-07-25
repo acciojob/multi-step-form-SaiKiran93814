ef102eb4fd62b3e9ac30b4940fab6b374b777718
SaiKiran93814 <teppali123@gmail.com>
2025-07-26 00:55:45 +0530

    Update code

--- src/components/Step.js
+++ src/components/Step.js
@@ -9,13 +9,12 @@ const Step = ({ step, formData, onChange, onNext, onPrevious, onSubmit }) => {
   return (
     <form className="form-box" onSubmit={onSubmit}>
       {step === 1 && (
-        <>
+        <div id="step1">
           <h2>Customer Details</h2>
           <p>First name:</p>
           <input
             id="first_name"
             type="text"
-            
             value={formData.first_name}
             onChange={onChange}
             required
@@ -24,46 +23,44 @@ const Step = ({ step, formData, onChange, onNext, onPrevious, onSubmit }) => {
           <input
             id="last_name"
             type="text"
-            
             value={formData.last_name}
             onChange={onChange}
             required
           />
-        </>
+        </div>
       )}
 
       {step === 2 && (
-        <>
+        <div id="step2">
           <h2>Car Details</h2>
           <p>Brand</p>
           <input
             id="model"
             type="text"
-            
             value={formData.model}
             onChange={onChange}
             required
           />
-          <p>Model</p>
+          <p>Price</p>
           <input
             id="car_price"
             type="number"
-            
             value={formData.car_price}
             onChange={onChange}
             required
           />
-        </>
+        </div>
       )}
 
       {step === 3 && (
-        <>
+        <div id="step3">
           <h2>Payment Details</h2>
-            <p>Credit Card Number:</p>
+          <p>Credit Card Number:</p>
           <input
             id="card_info"
             type="text"
-            
+            inputMode="numeric"
+            maxLength={12}
             value={formData.card_info}
             onChange={onChange}
             required
@@ -71,19 +68,19 @@ const Step = ({ step, formData, onChange, onNext, onPrevious, onSubmit }) => {
           {formData.card_info.length > 0 && formData.card_info.length !== 12 && (
             <p className="warning">Credit card number must be exactly 12 digits long</p>
           )}
-            <p>Expiration Date:</p>
+          <p>Expiration Date:</p>
           <input
             id="expiry_date"
             type="text"
-            
+            placeholder="MM/YY"
             value={formData.expiry_date}
             onChange={onChange}
             required
           />
           {formData.expiry_date.length > 0 && !isValidExpiry(formData.expiry_date) && (
-            <p className="warning">Expiration Date must be in the formate MM/YY</p>
+            <p className="warning">Expiration Date must be in the format MM/YY</p>
           )}
-        </>
+        </div>
       )}
 
       <div className="btn-group">
